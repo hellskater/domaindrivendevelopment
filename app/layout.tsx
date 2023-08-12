150b58cd6fab2945fc3daa5e890458644892722d
K Srinivas Rao <ksrinivasrao531@gmail.com>
2023-08-12 19:13:23 +0530

    content

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import clsx from "clsx";
 import { Analytics } from "@vercel/analytics/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Domain Driven Development",
@@ -70,7 +70,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={clsx("bg-black text-white", inter.className)}>
+    <html
+      lang="en"
+      className={clsx("bg-black text-lg text-white", inter.className)}
+    >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,57 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <section>
+      <h1 className="font-bold text-3xl">
+        Domains: The first step to...well something
+      </h1>
+      <p className="mt-10">
+        Welcome to the limbo of the internet, the graveyard of ambition. Here,
+        domains float aimlessly, each waiting for its moment of glory. Some will
+        rise, transforming into the face of groundbreaking websites, while
+        others? Well, they&apos;ll remain tokens of what could have been. Tokens
+        of those overly optimistic Friday nights when you thought, &quot;I have
+        an idea!&quot; and promptly bought a domain. Because, as we all know,
+        &apos;I have a domain&apos; is the new &apos;I have an idea&apos;.
+      </p>
+      <p className="mt-10">
+        Every great journey, a &quot;Hero&apos;s&quot; journey, begins with a
+        single step. In the wild jungle of the internet, your first baby step is
+        basically buying a domain. Like, congrats on the big move, digital
+        pioneer! It&apos;s a commitment, a silent pledge to oneself. &quot;This
+        is it&quot; you think, the glint of ambition in your eyes, &quot;the
+        start of my next big project.&quot; But let&apos;s be real: sometimes,
+        it&apos;s just the beginning of an ever-growing collection of domains,
+        each one a poignant reminder of dreams deferred and ideas that never
+        quite took flight.
+      </p>
+      <p className="mt-10">
+        There&apos;s an undeniable art to procrastination. It&apos;s not merely
+        about pushing tasks to tomorrow; it&apos;s about preparing, planning,
+        and... purchasing domains. After all, nothing screams &quot;I&apos;m
+        totally going to start that project&quot; louder than buying a domain
+        name. It&apos;s the digital equivalent of buying a new notebook for a
+        project. The pages might be empty, but oh, the potential they hold! And
+        while every domain might promise endless possibilities, the road to
+        success, it seems, is paved with these unused domains. Welcome to the
+        parking lot.
+      </p>
+      <p className="mt-10">
+        But let&apos;s not be too harsh. Domains are the footprints we leave in
+        the sands of the digital realm. They&apos;re markers of our ambition,
+        our hopes, and sometimes, just our impulsive nature after three cups of
+        coffee. Whether they lead to bustling online metropolises or remain
+        solitary markers in the vast desert of the web, they&apos;re a testament
+        to the spirit of creation. Remember: A domain in hand is worth two in
+        the bush... or something like that.
+      </p>
+      <p className="mt-10">
+        So, got a domain but no product? You&apos;re in the right place. Why
+        bother building a product when the mere act of buying a domain feels so
+        accomplished? After all, every domain deserves its 15 minutes of fame,
+        even if it&apos;s just here. So, leave your domain at the door, and
+        let&apos;s revel in the limitless potential of what could be... or
+        probably never will be. No pressure.
+      </p>
+    </section>
   );
 }
